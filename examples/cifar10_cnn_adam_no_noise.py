31b884583d7dc5ad8245f5badaa574f99c46ac2e
mirfan899 <virtuoso.irfan@gmail.com>
2018-12-18 17:37:18 +0500

    some changes

--- examples/cifar10_cnn_adam_no_noise.py
+++ examples/cifar10_cnn_adam_no_noise.py
@@ -19,7 +19,7 @@ from keras.utils import plot_model
 
 batch_size = 32
 num_classes = 10
-epochs = 100
+epochs = 200
 data_augmentation = True
 num_predictions = 20
 save_dir = os.path.join(os.getcwd(), 'saved_models')
@@ -36,28 +36,25 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = Sequential()
-model.add(Conv2D(64, (2, 2), padding='same',
-                 input_shape=x_train.shape[1:], name="input_layer"))
+model.add(Conv2D(32, (3, 3), padding='same',
+                 input_shape=x_train.shape[1:]))
 model.add(Activation('relu'))
-model.add(Conv2D(128, (2, 2), name="second"))
+model.add(Conv2D(32, (3, 3)))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(1, 1)))
-model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-model.add(Conv2D(256, (2, 2), padding='same', name="third"))
+model.add(Conv2D(64, (3, 3), padding='same'))
 model.add(Activation('relu'))
-model.add(AveragePooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
-
-model.add(Conv2D(512, (2, 2), name="fourth"))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
-model.add(AveragePooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.2))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(1024))
+model.add(Dense(512))
 model.add(Activation('relu'))
-model.add(Dropout(0.2))
+model.add(Dropout(0.5))
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
 
