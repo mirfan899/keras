a7b26909f516c9da5aba2695bc3328de8f192569
mirfan899 <virtuoso.irfan@gmail.com>
2018-12-18 17:21:10 +0500

    epoch 200

--- examples/cifar10_cnn_adam_3_layer_noise.py
+++ examples/cifar10_cnn_adam_3_layer_noise.py
@@ -47,7 +47,7 @@ class Gaussian(keras.callbacks.Callback):
 
 batch_size = 32
 num_classes = 10
-epochs = 100
+epochs = 200
 data_augmentation = True
 num_predictions = 20
 save_dir = os.path.join(os.getcwd(), 'saved_models')
